c23a1c4382dde874fff452dca234585cf1bdd744
Thomas Holmes <thomasholmes@outlook.com>
2018-09-22 18:00:05 +0100

    Added basic image grab endpoint.

--- src/main/java/com/github/richteaman/bot/VideoController.java
+++ src/main/java/com/github/richteaman/bot/VideoController.java
@@ -1,28 +1,50 @@
 package com.github.richteaman.bot;
 
+import com.github.sarxos.webcam.Webcam;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import org.springframework.core.io.Resource;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 @Controller
 public class VideoController {
+
+    private Webcam webcam = Webcam.getDefault();
+
+    public VideoController() {
+
+
+        Dimension dimension = new Dimension(320, 240);
+        webcam.setViewSize(dimension);
+
+        webcam.open();
+    }
 
     @GetMapping(path = "/plain", produces = "video/mp4")
     @ResponseBody
     public FileSystemResource plain() {
 
         return new FileSystemResource("D:/Projects/Bot/Test.mp4");
+    }
+
+    @GetMapping(path = "/cam", produces = "image/jpg")
+    @ResponseBody
+    public ByteArrayResource cam(@RequestHeader HttpHeaders headers)
+            throws IOException {
+
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(webcam.getImage(), "JPG", baos);
+
+        return new ByteArrayResource(baos.toByteArray());
     }
 
 }
