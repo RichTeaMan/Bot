dfecf76f70af09285d2ce6ac08087e139fad98f5
Thomas Holmes <thomasholmes@outlook.com>
2018-09-30 17:30:05 +0100

    Added FPS slider.

--- src/main/webapp/resources/script/bot.js
+++ src/main/webapp/resources/script/bot.js
@@ -1,8 +1,26 @@
-function beginVideo() {
-    setInterval(function(){ populateGameGrid("videoImg"); }, 100);
+var videoInterval = null;
+
+function createVideoStream(fps) {
+    if (null != videoInterval) {
+        clearInterval(videoInterval);
+    }
+    var interval = 1000 / fps;
+    videoInterval = setInterval(function(){ fetchVideoImage("videoImg"); }, interval);
+
+    updateInput('fpsTextInput', fps);
+    updateInput('fpsRangeInput', fps);
 }
 
-function populateGameGrid(destinationElementId) {
-
+function fetchVideoImage(destinationElementId) {
     $(`#${destinationElementId}`).attr("src", "/cam?" + new Date().getTime());
+}
+
+function updateInput(textInput, val) {
+    var element = $(`#${textInput}`);
+    if (element) {
+        var oldVal = element.val();
+        if (oldVal !== val) {
+            element.val(val);
+        }
+    }
 }
